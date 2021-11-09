e4697e3849cf2d8ecff2cfee0f3883b47db51eaa
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-11-09 13:27:03 +0100

    fix save path

--- RFunction.R
+++ RFunction.R
@@ -20,7 +20,7 @@ rFunction <- function(data,variab,rel,valu,minlocs=1,minanimals=1,time=FALSE,ema
             nloc <- length(selix)
             nani <- length(unique(as.data.frame(data)$trackId[selix]))
             logger.info(paste("Your required alert property:",variab," ",rel," [",valu,"] is fulfilled by",nloc,"locations of",nani,"animals. An Email Alert txt file will be generated. The full data set will be passed on as output."))
-            if (nloc>=minlocs & nani>=minanimals) writeLines(c(emailtext,paste("Your following Alert Condition is fullfilled:",variab,rel,valu,"for", nloc, ">=",minlocs,"locations of", nani, ">=", minanimals,"animals.")), "email_alert_text.txt")
+            if (nloc>=minlocs & nani>=minanimals) writeLines(c(emailtext,paste("Your following Alert Condition is fullfilled:",variab,rel,valu,"for", nloc, ">=",minlocs,"locations of", nani, ">=", minanimals,"animals.")), paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"email_alert_text.txt"))
           } else 
           {
             logger.info("None of your data fulfill the required alert property. No alert artefact is written.")
@@ -33,7 +33,7 @@ rFunction <- function(data,variab,rel,valu,minlocs=1,minanimals=1,time=FALSE,ema
             nloc <- length(which(fullrel==TRUE))
             nani <- length(unique(as.data.frame(data)$trackId[which(fullrel==TRUE)]))
             logger.info(paste("Your required alert property:",variab," ",rel," [",valu,"] is fulfilled by",nloc,"locations of",nani,"animals. An Email Alert txt file will be generated. The full data set will be passed on as output."))
-            if (nloc>=minlocs & nani>=minanimals) writeLines(c(emailtext,paste("Your following Alert Condition is fullfilled:",variab,rel,valu,"for", nloc, ">=",minlocs,"locations of", nani, ">=", minanimals,"animals.")), "email_alert_text.txt")
+            if (nloc>=minlocs & nani>=minanimals) writeLines(c(emailtext,paste("Your following Alert Condition is fullfilled:",variab,rel,valu,"for", nloc, ">=",minlocs,"locations of", nani, ">=", minanimals,"animals.")), paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"email_alert_text.txt"))
           } else  logger.info("None of your data fulfill the required property. No alert artefact is written.")
         }
       } else logger.info("You selected variable is not available in the data set. Please also check your spelling (Cargo Agent of previous App). Go back and reconfigure the App.")
